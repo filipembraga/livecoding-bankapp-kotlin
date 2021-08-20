e88fc7dd90e718c4748f28d6ccf712d1f939a052
filipembraga <lipebraga16@hotmail.com>
2021-08-19 22:36:05 -0300

    First version of project

---
+++ app/src/main/java/com/example/diolabs_bank_app/MainActivity.kt
@@ -0,0 +1,55 @@
+package com.example.diolabs_bank_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var recycledViewList: RecyclerView
+    private var adapter = MenuItemAdapter()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startViews()
+        setItemsList()
+    }
+
+    private fun startViews(){
+        recycledViewList = findViewById(R.id.rv_list)
+        recycledViewList.adapter = adapter
+        recycledViewList.layoutManager = GridLayoutManager(this,2)
+    }
+
+    private fun setItemsList() {
+        adapter.setItensList(
+            arrayListOf(
+                MenuItemModel(
+                    "cartões"
+                ),
+                MenuItemModel(
+                    "meus comprovantes"
+                ),
+                MenuItemModel(
+                    "investimentos"
+                ),
+                MenuItemModel(
+                    "portabilidade de salário"
+                ),
+                MenuItemModel(
+                    "cartões"
+                ),
+                MenuItemModel(
+                    "meus comprovantes"
+                ),
+                MenuItemModel(
+                    "investimentos"
+                ),
+                MenuItemModel(
+                    "portabilidade de salário"
+                )
+            )
+        )
+    }
+}
---
+++ app/src/main/java/com/example/diolabs_bank_app/MenuItemModel.kt
@@ -0,0 +1,5 @@
+package com.example.diolabs_bank_app
+
+class MenuItemModel (
+    val title: String
+)
